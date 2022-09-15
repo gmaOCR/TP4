f5cd4223c5fbeb7c59f578a8bfdcf251f6b161f2
Greg <marianigregory@pm.me>
2022-09-15 10:26:55 +0200

    15.09.22

--- modules/Controller.py
+++ modules/Controller.py
@@ -3,19 +3,9 @@ from Models import Tournament
 from Models import Player
 
 
-#Lignes de test
 
-players_list = {Player("A", "1", "01/01/01", "M", "100", 1000), Player("B", "2", "02/02/02", "M", "95", 1001),
-               Player("C","3", "03/03/03","M","103", 1002), Player("D", "4", "04/04/04", "F", "140", 1003),
-               Player("E", "5", "05/05/05", "M", "65", 1004), Player("F", "6", "06/06/06", "F", "233", 1005),
-               Player("G", "7", "07/07/07", "M", "230", 1006), Player("H", "8", "08/08/08", "F", "324", 1007)}
 
-for players in players_list:
-    print(players.display_player())
-"""Génère 8 joueurs provisoires"""
 
-tournament = Tournament("Tournoi de la rue du Stand","Genève","05/09/22","Blitz",4,players_list,"" )
-print(tournament.display_tournament())
 
 
 
---
+++ modules/RoundManager.py
@@ -0,0 +1,29 @@
+
+from Models import Round
+
+def add_player_to_round(self, player):
+    """Ajout des joueurs au round"""
+    Round.match_list.append(player)
+
+def generate_match(self, playerA, playerB):
+    """A coder"""
+    """Stocker sous forme de tuple en 2 listes chaque match"""
+    """Liste 1 ref à une instance de Player"""
+    """Liste 2 score du match"""
+    pass
+
+def run_first_round(self, playerA, playerB):
+    """A coder"""
+    """Premier tour en fonction du Player rank soit:"""
+    """N1 vs N5 et N2 vs N6 et N3 vs N7 et N4 vs N8"""
+    pass
+
+def run_next_round(self):
+    """A coder"""
+    """A partir du tour 2 - Fonction du score total et (rank si nécessaire)"""
+    """Puis N1 vs N2 etc.. sauf si VS a déjà eu lieu"""
+    pass
+
+def save_current_round(self):
+    """A coder"""
+    pass   
