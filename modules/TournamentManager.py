7e19fa1c2d4b903ca3484d38870da215b093900c
Greg M <marainigregory@pm.me>
2022-09-18 12:18:22 +0200

    18.09.22

--- modules/TournamentManager.py
+++ modules/TournamentManager.py
@@ -1,22 +1,26 @@
-from modules.Models import Tournament
+import modules.View
+from modules.Models import Tournament, Player
 from modules.View import Menus
+
+TIME_CONTROL = modules.View.TIME_CONTROL
+
+menu = Menus()
 
 class TournamentManager:
 
+
     def get_info(self):
-        menu = Menus()
         """Déclare l'objet "Menus" """
         menu.hello()
         """Message de bienvenue"""
-        choice = menu.select_menu(menu.main_menu())
+        choice = menu.get_input_str(menu.main_menu())
         """Menu principal"""
         while choice not in ["1","2","3","4"]:
             print("\n Choix incorrect\n")
-            choice = menu.select_menu(menu.main_menu())
+            choice = menu.get_input_str(menu.main_menu())
         if choice == "1":
-            """ Créer un tournoi"""
-            tournament_input = menu.create_tournament("Entrez le nom du tournoi:")
-           # tournament = Tournament()
+            """ Créer un tournoi et récupère ses propriétés"""
+            self.create_tournament()
         elif choice == "2":
             """ Ajouter des joueurs à la database"""
             pass
@@ -27,8 +31,29 @@ class TournamentManager:
             """ Quitter le programme """
             exit
 
+
     def create_tournament(self):
-        tournament = Tournament()
+        t_name = menu.get_input_str("Entrez le nom du tournoi:")
+        t_place = menu.get_input_str("Entrez le lieu du tournoi:")
+        t_date = menu.get_input_str("Entrez la date du tournoi: (JJ/MM/AAAA)")
+        t_rounds = menu.get_input_int("Entrez le nombre de rondes: (défaut 4)")
+        if t_rounds is None:
+            t_rounds = 4
+        t_time_control = menu.get_time_control("Entrez le type de chrono: (1. bullet 2. blitz 3. coup rapide)")
+        while t_time_control > len(TIME_CONTROL):
+            print("\n Choix incorrect\n")
+            t_time_control = menu.get_time_control(menu.tc_out_of_range())
+        """Vérifier et récupère le type de chrono dans la constante TIME_CONTROL"""
+        if t_time_control == 1:
+            t_time_control = TIME_CONTROL[0]
+        if t_time_control == 2:
+            t_time_control = TIME_CONTROL[1]
+        if t_time_control == 3:
+            t_time_control = TIME_CONTROL[2]
+        else:
+            menu.get_time_control(menu.tc_out_of_range())
+        t_desc = menu.get_input_str("Entre un commentaire (facultatif):")
+        #tournament = Tournament()
 
     def display_tournament(self):
         return (f"Nom du tournoi: {self.name}\n"
--- modules/View.py
+++ modules/View.py
@@ -1,5 +1,7 @@
 import datetime
 
+TIME_CONTROL = ["1. bullet", "2. blitz", "3. coup rapide"]
+
 class Menus:
 
     def hello(self):
@@ -11,20 +13,38 @@ class Menus:
                 "3.Consulter des informations \n"
                 "4.Quitter \n")
 
-    def select_menu(self, menu):
+    def get_input_str(self, menu):
         choice = input(menu)
         return choice
 
+    def get_input_int(self, menu):
+        if input(menu) == "":
+            return None
+        else:
+            while ValueError is not None:
+                try:
+                    choice = input(menu)
+                    return int(choice)
+                except ValueError:
+                    print("Le nombre de rounds doit être un nombre entier")
+
+    def get_time_control(self, menu):
+        while ValueError is not None:
+            try:
+                choice = input(menu)
+                return int(choice)
+            except ValueError:
+                return Menus.tc_out_of_range()
+
+
+    def tc_out_of_range(self):
+        """Retourne le menu de choix de TIMECONTROL """
+        return("Entrez le chiffre correspondant:\n"
+                + print(*TIME_CONTROL, sep = "\n"))
+
     def create_tournament(self):
-            name = input()
-            """ Nom du tournoi """
-            place = input("Entrez le lieu du tournoi:")
-            """" Lieu du tournoi """
-            date = input("Entrez la date du tournoi: (JJ/MM/AAAA)")
-            """ Date du jour du tournoi"""
-            rounds = input("Entrez le nombre de rondes: (défaut 4)")
-            desc = input("Entre un commentaire (facultatif):")
-            return [name, place, date, rounds, desc]
-            
+        """A coder"""
+        pass
+
 
 
